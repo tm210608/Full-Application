55ef3d08439ef9873ee50b4f827378599fb23c1f
Miguel Angel Gijon Miranda <miguel.gijon@globant.com>
2024-08-08 11:49:20 +0200

    Fixing Branch Tony Subiendo la rama modificada

--- login/src/main/java/com/mito/login/ui/LoginViewModel.kt
+++ login/src/main/java/com/mito/login/ui/LoginViewModel.kt
@@ -2,10 +2,9 @@ package com.mito.login.ui
 
 import android.util.Log
 import android.util.Patterns
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.mito.common.tools.EMPTY_STRING
 import com.mito.common.usecase.Result
 import com.mito.login.domain.DummyLoginUseCase
 import com.mito.login.domain.Input
@@ -13,6 +12,7 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -21,25 +21,30 @@ class LoginViewModel @Inject constructor(
     private val dummyLoginUseCase: DummyLoginUseCase,
 ) : ViewModel() {
 
-    private val _status: MutableStateFlow<Status> = MutableStateFlow(Status(USERNAME, PASSWORD))
+    private val _status: MutableStateFlow<Status> = MutableStateFlow(Status())
     val status: StateFlow<Status> = _status
 
     private val _event: MutableStateFlow<Event> = MutableStateFlow(Event.None)
     val event: StateFlow<Event> = _event
 
 
-    fun login(username: String, password: String) {
+    fun login() {
         viewModelScope.launch {
-            dummyLoginUseCase(Input(username, password))
+            dummyLoginUseCase(Input(status.value.username, status.value.password))
+                .onStart {
+                    _event.emit(Event.Loading)
+                }
                 .collect {
                     when (it) {
                         is Result.Error -> {
                             Log.d("Login MITO", "No funcionó el Login")
+                            delay(2000)
                             _event.emit(Event.Error(it.toString()))
                         }
 
                         is Result.Success<*> -> {
                             Log.d("Login MITO", "Funcionó el Login")
+                            delay(2000)
                             _event.emit(Event.Success(it.value.toString()))
                         }
                     }
@@ -47,15 +52,12 @@ class LoginViewModel @Inject constructor(
         }
     }
 
-    private val _loginEnable = MutableLiveData<Boolean>()
-    val loginEnable: LiveData<Boolean> = _loginEnable
-
-    private val _isLoading = MutableLiveData<Boolean>()
-    val isLoading: LiveData<Boolean> = _isLoading
-
     fun onLoginChanged(email: String, password: String) {
-        _status.value = Status(email, password)
-        _loginEnable.value = isValidEmail(email) && isValidPassword(password)
+        _status.value = _status.value.copy(
+            username = email,
+            password = password,
+            loginEnable = isValidEmail(email) && isValidPassword(password)
+        )
     }
 
     private fun isValidPassword(password: String): Boolean = password.length > 6
@@ -63,34 +65,25 @@ class LoginViewModel @Inject constructor(
     private fun isValidEmail(email: String): Boolean =
         Patterns.EMAIL_ADDRESS.matcher(email).matches()
 
-    suspend fun onLoginSelected() {
-        _isLoading.value = true
-        delay(4000)
-        _event.emit(Event.Success("Login Successfully"))
-        delay(3000)
-        _event.emit(Event.None)
-        delay(1000)
-        _isLoading.value = false
-    }
-
-    sealed class Event {
-        data object Loading : Event()
-        data class Success(val message: String) : Event()
-        data class Error(val message: String) : Event()
-        data object None : Event()
-    }
-
-    data class Status(
-        val username: String,
-        val password: String,
-    )
-
-    companion object {
-        private const val USERNAME = ""
-        private const val PASSWORD = ""
-    }
-
     fun clearEvent() {
         viewModelScope.launch { _event.emit(Event.None) }
     }
+
+    fun isLoading(event: Event) {
+        _status.value = status.value.copy(isLoading = event is Event.Loading)
+    }
 }
+
+sealed class Event {
+    data object Loading : Event()
+    data class Success(val message: String) : Event()
+    data class Error(val message: String) : Event()
+    data object None : Event()
+}
+
+data class Status(
+    val username: String = EMPTY_STRING,
+    val password: String = EMPTY_STRING,
+    val loginEnable: Boolean = false,
+    val isLoading: Boolean = false
+)
