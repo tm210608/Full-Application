46986143549d24ac43486253d9b916ea074e150b
Miguel Angel Gijon Miranda <miguel.gijon@globant.com>
2024-08-28 14:52:50 +0200

    Adding disable state for primary button

--- libs/components/src/main/java/com/mito/components/Buttons.kt
+++ libs/components/src/main/java/com/mito/components/Buttons.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.mito.components.resources.primary_button_color_container
+import com.mito.components.resources.primary_button_color_container_disabled
 import com.mito.components.resources.primary_button_color_content
+import com.mito.components.resources.primary_button_color_content_disabled
 import com.mito.components.resources.primary_button_corner_radius
 import com.mito.components.resources.primary_button_font_size
 import com.mito.components.resources.primary_button_height
@@ -34,8 +36,8 @@ fun PrimaryButton(
         colors = ButtonDefaults.buttonColors(
             containerColor = primary_button_color_container,
             contentColor = primary_button_color_content,
-            disabledContainerColor = primary_button_color_container,
-            disabledContentColor = primary_button_color_content
+            disabledContainerColor = primary_button_color_container_disabled,
+            disabledContentColor = primary_button_color_content_disabled
         ),
         enabled = isEnabled,
         shape = RoundedCornerShape(primary_button_corner_radius)
